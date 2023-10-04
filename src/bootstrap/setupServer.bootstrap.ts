68034987a95d3c1bf0cfd3573c88e7abc27db3a6
dev-miguel-resources <ingenieromiguelch@gmail.com>
2023-10-03 21:19:36 -0300

    new server configs and first steps with config exceptions handlers

--- src/bootstrap/setupServer.bootstrap.ts
+++ src/bootstrap/setupServer.bootstrap.ts
@@ -1,9 +1,15 @@
 import { Application, json, urlencoded, Request, Response } from 'express';
 import http from 'http';
 import cors from 'cors';
+import helmet from 'helmet';
+import hpp from 'hpp';
+import compression from 'compression';
 import cookieSession from 'cookie-session';
+import 'express-async-errors';
 import { config } from '@configs/configEnvs';
 
+// SINGLE RESPONSABILITY: S
+// OPEN/CLOSED: O
 export class RedSocialServer {
 	private app: Application;
 
@@ -13,6 +19,8 @@ export class RedSocialServer {
 
 	public start(): void {
 		// definimos lo que va a ejecutar el servidor cuando se levante
+		this.securityMiddleware(this.app);
+		this.standardMiddleware(this.app);
 	}
 
 	private securityMiddleware(app: Application): void {
@@ -24,6 +32,28 @@ export class RedSocialServer {
 				secure: config.NODE_ENV !== 'development'
 			})
 		);
+		app.use(hpp());
+		app.use(helmet());
+	  app.use(cors({
+			origin: config.CLIENT_URL,
+			credentials: true,
+			optionsSuccessStatus: 200,
+			methods: ['GET', 'POST', 'PUT', 'DELETE']
+		})
+		);
+	}
+
+	private standardMiddleware(app: Application): void {
+		app.use(compression());
+		app.use(json({ limit: '50mb' }));
+		app.use(urlencoded({ extended: true, limit: '50mb' }));
+	}
+
+	private routeMiddleware(app: Application): void {
+		// reconocimiento de rutas
+	}
+
+	private globalErrorHandler(app: Application): void {
 
 	}
 }
---
+++ src/shared/globals/helpers/errors/customError.ts
@@ -0,0 +1,18 @@
+import { IError } from './interfaces/error.interface';
+
+export abstract class CustomError extends Error {
+	abstract statusCode: number;
+	abstract status: string;
+
+	constructor(message: string) {
+		super(message);
+	}
+
+	serializeErrors(): IError {
+		return {
+			message: this.message,
+			status: this.status,
+			statusCode: this.statusCode
+		};
+	}
+}
